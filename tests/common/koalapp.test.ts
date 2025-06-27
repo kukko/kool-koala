9ff7c5c45e31946227e7a917db90f0923b2075e3
kukko <nyariag@gmail.com>
2025-06-27 08:14:45 +0200

    fix(test): add test for serving images from static folders

--- tests/common/koalapp.test.ts
+++ tests/common/koalapp.test.ts
@@ -6,6 +6,7 @@ import { MockConfigurationParameters } from '../mocks/mock-configuration-paramet
 import axios from 'axios';
 
 import { join } from 'path';
+import fs from 'fs';
 
 describe('KoalApp', () => {
   it('should be defined', () => {
@@ -30,14 +31,27 @@ describe('KoalApp', () => {
       });
     });
 
-    it('Serves static file', (done) => {
-      axios.get(`http://localhost:${MockConfigurationParameters.port}/test.txt`, {
-        validateStatus: () => true
-      }).then((response) => {
-        expect(response.status).to.equal(200);
-        expect(response.data).to.equal('It is working! Kool!');
-        done();
-      }).catch(done);
+    describe('Serves static files', () => {
+      it('Serves text file', (done) => {
+        axios.get(`http://localhost:${MockConfigurationParameters.port}/test.txt`, {
+          validateStatus: () => true
+        }).then((response) => {
+          expect(response.status).to.equal(200);
+          expect(response.data).to.equal('It is working! Kool!');
+          done();
+        }).catch(done);
+      });
+      it('Serves image', (done) => {
+        console.log(`http://localhost:${MockConfigurationParameters.port}/koala.png`);
+        axios.get(`http://localhost:${MockConfigurationParameters.port}/koala.png`, {
+          validateStatus: () => true
+        }).then((response) => {
+          expect(response.status).to.equal(200);
+          expect(response.headers['content-type']).to.equal('image/png');
+          expect(parseInt(response.headers['content-length'])).to.equal(fs.statSync(join(__dirname, '../assets/koala.png')).size);
+          done();
+        }).catch(done);
+      });
     });
 
     after(async () => {
